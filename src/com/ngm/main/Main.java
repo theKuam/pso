0eef495df25204da28584a508096191a28b805cd
theKuam <ngominh.hai96@gmail.com>
2019-02-17 20:49:29 +0700

    clean up a little.

--- src/com/ngm/main/Main.java
+++ src/com/ngm/main/Main.java
@@ -4,10 +4,10 @@ import com.ngm.tsp.CitySwarm;
 
 public class Main {
     public static void main(String[] args) {
-	// write your code here
-    CitySwarm.initializeMap();
-    CitySwarm.printMap();
-    CitySwarm.PSOAlgorithm();
-    CitySwarm.printBestSolution();
+        // write your code here
+        CitySwarm.initializeMap();
+        CitySwarm.printMap();
+        CitySwarm.PSOAlgorithm();
+        CitySwarm.printBestSolution();
     }
 }
